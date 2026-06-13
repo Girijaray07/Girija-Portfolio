4e5a08c9be8731b224e218215f2215db56e4c392
Girija Ray <91793906+Girijaray07@users.noreply.github.com>
2026-06-13 15:35:07 +0530

    Main Layout of the Pages

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'motion/react';
+import HeaderElement from '../components/Header/HeaderElement';
+import FooterElement from '../components/Footer/FooterElement';
+import CircleCursor from '../hooks/CircleCursor';
+import ScrollToTop from '../components/ScrollToTop';
+
+const MainLayout = () => {
+  const location = useLocation();
+
+  return (
+    <div className="main-layout">
+      <ScrollToTop />
+      <HeaderElement />
+      <main className="portfolio-content">
+        <AnimatePresence mode="wait">
+          <Outlet key={location.pathname} />
+        </AnimatePresence>
+      </main>
+      <FooterElement />
+      <CircleCursor
+        size={24}
+        borderWidth={5}
+        color="rgba(255, 0, 0, 0.8)"
+        trailColor="rgba(255, 0, 0, 0.2)"
+        delay={0.12}
+      />
+    </div>
+  );
+};
+
+export default MainLayout;
